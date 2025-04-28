7967aae298b1d6eeb9cdcaf83bc7b68832aa0c8b
heejoon lee <leehj7739@gmail.com>
2025-04-28 13:55:23 +0900

    REPO ERROR

--- files/variables.tf
+++ files/variables.tf
@@ -120,5 +120,5 @@ variable "security_group_name" {
 variable "app_repository" {
   description = "애플리케이션 저장소 URL"
   type        = string
-  default     = "https://github.com/yourusername/your-fastapi-app.git"
+  default     = https://github.com/leehj7739/git_terraform.git
 } 
