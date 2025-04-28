a5bb268e6f8a4a206f31965430fd60c0710e88bc
heejoon lee <leehj7739@gmail.com>
2025-04-28 19:44:50 +0900

    h2

--- .github/lab-terraform-modules/outputs.tf
+++ .github/lab-terraform-modules/outputs.tf
@@ -18,15 +18,6 @@ output "instance_addresses" {
   value       = var.create_instance ? module.web_server[*].instance_addresses : []
 }
 
-output "data_volume_id" {
-  description = "생성된 데이터 볼륨의 ID"
-  value       = var.create_data_volume ? openstack_blockstorage_volume_v3.data[0].id : null
-}
-
-output "storage_container_name" {
-  description = "생성된 오브젝트 스토리지 컨테이너 이름"
-  value       = var.create_s3_bucket ? openstack_objectstorage_container_v1.storage[0].name : null
-}
 
 output "lb_vip" {
   description = "로드밸런서 VIP 주소"
