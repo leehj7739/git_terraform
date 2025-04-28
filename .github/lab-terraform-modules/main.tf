3a1a33c4968d79521ccc43c1e386bf967623b834
heejoon lee <leehj7739@gmail.com>
2025-04-28 17:34:14 +0900

    1

--- .github/lab-terraform-modules/main.tf
+++ .github/lab-terraform-modules/main.tf
@@ -155,9 +155,3 @@ resource "openstack_lb_member_v2" "blue_members" {
   protocol_port = 80
   subnet_id     = var.subnet_id
 }
-
-# 로드밸런서 퍼블릭 IP 출력
-output "loadbalancer_public_ip" {
-  description = "로드밸런서의 퍼블릭 IP 주소"
-  value       = "210.109.82.75"
-}
