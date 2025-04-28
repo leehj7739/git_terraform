05c19423e5bfddc927229b409543183f66eea7b1
heejoon lee <leehj7739@gmail.com>
2025-04-28 14:46:37 +0900

    REPO ERROR

--- files/main.tf
+++ files/main.tf
@@ -55,28 +55,6 @@ resource "openstack_networking_secgroup_rule_v2" "web_http" {
   security_group_id = openstack_networking_secgroup_v2.web.id
 }
 
-# 인스턴스 생성
-resource "openstack_compute_instance_v2" "web" {
-  count           = var.create_instance ? 1 : 0
-  name            = "${var.dev_name}-web-server"
-  image_id        = var.image_id
-  flavor_name     = var.flavor_name
-  key_pair        = var.key_name
-  security_groups = [openstack_networking_secgroup_v2.web.name]
-
-  network {
-    name = var.network_name
-  }
-
-  block_device {
-    uuid                  = var.image_id
-    source_type           = "image"
-    volume_size           = var.root_volume_size
-    boot_index            = 0
-    destination_type      = "volume"
-    delete_on_termination = true
-  }
-}
 
 # EBS 볼륨 생성 (선택적)
 resource "openstack_blockstorage_volume_v3" "data" {
